da956bcb0b874fa8d0e08c98fc5eb39c5e7e7f9b
狮子的魂 <chenxin619315@gmail.com>
2015-11-28 23:22:46 +0800

    add sentence segementation

---
+++ jcseg-core/src/main/java/org/lionsoul/jcseg/SentenceSeg.java
@@ -0,0 +1,169 @@
+package org.lionsoul.jcseg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.lionsoul.jcseg.util.IPushbackReader;
+import org.lionsoul.jcseg.util.IStringBuffer;
+
+/**
+ * document sentence splitter
+ * 
+ * @author chenxin<chenxin619315@gmail.com>
+*/
+public class SentenceSeg 
+{
+	private static final int MAX_QUOTE_LENGTH = 15;
+	
+	//current position for the specifield stream
+	protected int idx;
+	
+	//protected PushbackReader reader = null;
+	protected IPushbackReader reader = null;
+	
+	/**
+	 * global string buffer 
+	*/
+	protected IStringBuffer gisb = null;
+		
+	/**
+	 * construct method
+	 * 
+	 * @param  reader
+	 * @throws IOException 
+	*/
+	public SentenceSeg(Reader reader) throws IOException
+	{
+		gisb = new IStringBuffer();
+		reset(reader);
+	}
+	
+	public SentenceSeg() throws IOException
+	{
+		this(null);
+	}
+	
+	
+	/**
+	 * stream/reader reset.
+	 * 
+	 * @param input
+	 * @throws IOException
+	 */
+	public void reset( Reader input ) throws IOException
+	{ 
+		if ( input != null ) 
+			reader = new IPushbackReader(new BufferedReader(input));
+		idx = -1;
+	}
+	
+	/**
+	 * read the next char from the current position
+	 * 
+	 * @throws IOException 
+	 */
+	protected int readNext() throws IOException 
+	{	
+		int c = reader.read();
+		if ( c != -1 ) idx++;
+		return c;
+	}
+	
+	/**
+	 * get the next sentence
+	 * 
+	 * @return	Sentence
+	 * @throws	IOException 
+	*/
+	public Sentence next() throws IOException
+	{
+		gisb.clear();
+		int c, pos = -1;
+		
+		while ( (c = readNext()) != -1 ) 
+		{
+			boolean endTag = false;
+			if ( pos == -1 ) pos = idx;
+			
+			/*
+			 * here, define the sentence end tag
+			 * punctuation like the following:
+			 * .。\n;；?？!！:： 
+			*/
+			switch ((char)c)
+			{
+			case '"': gisb.append('"'); readUntil('"');  break;
+			case '“': gisb.append('"'); readUntil('”');  break;
+			case '【': gisb.append('"'); readUntil('】'); break;
+			case '《': gisb.append('"'); readUntil('》'); break;
+			case '.':
+			case '。':
+			case ';':
+			case '；':
+			case '?':
+			case '？':
+			case '!':
+			case '！':
+			{
+				endTag = true;
+				gisb.append((char)c);
+				break;
+			}
+			case ':':
+			case '：':
+			case '\n':
+			{
+				endTag = true;
+				break;
+			}
+			default:
+				gisb.append((char)c);
+			}
+			
+			if ( endTag ) 
+			{
+				if ( gisb.length() <= 1 ) continue;
+				return new Sentence(gisb.toString(), pos);
+			}
+		}
+				
+		return null;
+	}
+	
+	/**
+	 * loop the reader until the specifield char is found.
+	 * 
+	 * @param	echar
+	 * @throws  IOException  
+	*/
+	protected void readUntil(char echar) throws IOException
+	{
+		int ch, i = 0;
+		IStringBuffer sb = new IStringBuffer();
+		while ( (ch = readNext()) != -1 )
+		{
+			if ( ++i >= MAX_QUOTE_LENGTH ) 
+			{
+				/*
+				 * push back the readed chars
+				 * and reset the global idx value. 
+				*/
+				for ( int j = sb.length() - 1; j >= 0; j-- ) {
+					reader.unread(sb.charAt(j));
+				}
+				idx -= sb.length();
+				break;
+			}
+			
+			sb.append((char)ch);
+			if ( ch == echar ) 
+			{
+				gisb.append(sb.toString());
+				break;
+			}
+		}
+		
+		sb = null;
+	}
+}
---
+++ jcseg-core/src/main/java/org/lionsoul/jcseg/test/SentenceSegTest.java
@@ -0,0 +1,36 @@
+package org.lionsoul.jcseg.test;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.lionsoul.jcseg.Sentence;
+import org.lionsoul.jcseg.SentenceSeg;
+
+/**
+ * sentence seg test program
+ * 
+ * @author chenxin<chenxin619315@gmail.com>
+*/
+public class SentenceSegTest 
+{
+
+	public static void main(String[] args) 
+	{
+		String doc = "冰岛时间7月1日，正在当地拍片的汤姆·克鲁斯通过发言人承认，他与第三任妻子凯蒂·赫尔墨斯（第一二任妻子分别为咪咪·罗杰斯、妮可·基德曼）的婚姻即将结束。"
+				+ "讽刺的是，3个女人都是在33岁离开这位“碟中谍”英雄的。"
+				+ "“三进三出”的婚姻令阿汤哥昔日“万人迷”的形象遭受严重冲击，也让公众不解，阿汤哥为什么找不到一个能长相依的爱人呢？"
+				+ "记者调查发现，阿汤哥的超强控制欲和生活中的种种“怪异行为”是导致其婚姻屡屡失败的原因。";
+	
+		try {
+			SentenceSeg seg = new SentenceSeg(new StringReader(doc));
+			Sentence sen = null;
+			while ( (sen = seg.next()) != null )
+			{
+				System.out.println(sen);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
