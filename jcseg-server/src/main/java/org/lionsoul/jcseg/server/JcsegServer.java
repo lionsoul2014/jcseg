18ae3dbfa5a0758bf552cd2124c637e123d8153e
chenxin <chenxin619315@gmail.com>
2015-12-04 21:51:56 +0800

    add jcseg-server.properties and properties encode program

--- jcseg-server/src/main/java/org/lionsoul/jcseg/server/JcsegServer.java
+++ jcseg-server/src/main/java/org/lionsoul/jcseg/server/JcsegServer.java
@@ -1,5 +1,8 @@
 package org.lionsoul.jcseg.server;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
@@ -8,6 +11,8 @@ import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.gzip.GzipHandler;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.lionsoul.jcseg.json.JSONArray;
+import org.lionsoul.jcseg.json.JSONObject;
 import org.lionsoul.jcseg.server.controller.MainController;
 import org.lionsoul.jcseg.server.controller.KeyphraseController;
 import org.lionsoul.jcseg.server.controller.KeywordsController;
@@ -18,6 +23,7 @@ import org.lionsoul.jcseg.server.core.AbstractRouter;
 import org.lionsoul.jcseg.server.core.DynamicRestRouter;
 import org.lionsoul.jcseg.server.core.ServerConfig;
 import org.lionsoul.jcseg.server.core.StandardHandler;
+import org.lionsoul.jcseg.tokenizer.Dictionary;
 import org.lionsoul.jcseg.tokenizer.core.ADictionary;
 import org.lionsoul.jcseg.tokenizer.core.DictionaryFactory;
 import org.lionsoul.jcseg.tokenizer.core.JcsegException;
@@ -132,40 +138,171 @@ public class JcsegServer
 	/**
 	 * register global resource (global resource initialize)
 	 * 
+	 * @param	globalConfig
 	 * @return JcsegServer
 	 * @throws CloneNotSupportedException 
 	 * @throws JcsegException 
 	*/
-	public JcsegServer registerGlobalResource() throws CloneNotSupportedException, JcsegException
+	public JcsegServer registerGlobalResource(JSONObject globalConfig) 
+			throws CloneNotSupportedException, JcsegException
 	{
-		String configFile = "/java/test/jcseg.properties";
-		JcsegTaskConfig tokenizerConfig = new JcsegTaskConfig(configFile);
-		JcsegTaskConfig extractorConfig = tokenizerConfig.clone();
-		
-		ADictionary dic = DictionaryFactory.createDefaultDictionary(tokenizerConfig);
-		
-		//segmentation object for extractor
-		extractorConfig.setAppendCJKPinyin(false);
-		extractorConfig.setClearStopwords(true);
-		extractorConfig.setKeepUnregWords(false);
+
+		//create a global JcsegTaskConfig and initialize from the global_setting
+		JcsegTaskConfig globalJcsegTaskConfig = new JcsegTaskConfig(null, false);
+		if ( globalConfig.has("global_config") ) {
+			JSONObject globalSetting = globalConfig.getJSONObject("global_config");
+			resetJcsegTaskConfig(globalJcsegTaskConfig, globalSetting);
+		}
 		
 		/*
-		 * register the global resource
+		 * create the dictionaris according to the defination of dict 
+		 * and we will make a copy of the globalSetting for dictionary load
+		 * 
+		 * reset the max length to pass the dictionary words length limitation
 		*/
-		resourcePool.addDict("main", dic);
-		resourcePool.addConfig("default", tokenizerConfig);
-		resourcePool.addConfig("extractor", extractorConfig);
+		JcsegTaskConfig dictLoadConfig = globalJcsegTaskConfig.clone();
+		dictLoadConfig.setMaxLength(100);
+		if ( globalConfig.has("dict") )
+		{
+			JSONObject dictSetting = globalConfig.getJSONObject("dict");
+			String[] dictNames = JSONObject.getNames(dictSetting);
+			for ( String name : dictNames )
+			{
+				JSONObject dicJson = dictSetting.getJSONObject(name);
+				if ( ! dicJson.has("path") ) {
+					throw new JcsegException("Missing path for dict instance " + name);
+				}
+				
+				JSONArray path = dicJson.getJSONArray("path");
+				if ( path.length() == 0 ) {
+					throw new JcsegException("Empty path setting for dict instance " + name);
+				}
+				
+				//process the lexPath
+				List<String> dicPath = new ArrayList<String>();
+				for ( int i = 0; i < path.length(); i++ ) {
+					dicPath.add(path.get(i).toString());
+				}
+				String[] lexPath = new String[dicPath.size()];
+				dicPath.toArray(lexPath);
+				dicPath.clear(); dicPath = null;
+				
+				boolean autoload = dicJson.has("autoload") 
+						? dicJson.getBoolean("autoload") : false;
+				int polltime = dicJson.has("polltime") 
+						? dicJson.getInt("polltime") : 300;
+				dictLoadConfig.setAutoload(autoload);
+				dictLoadConfig.setPollTime(polltime);
+				dictLoadConfig.setLexiconPath(lexPath);
+				
+				//create and register the global dictionary resource
+				ADictionary dic = DictionaryFactory.createDefaultDictionary(dictLoadConfig);
+				resourcePool.addDict(name, dic);
+			}
+		}
+		
+		dictLoadConfig = null;
 		
 		/*
-		 * register tokenizer entry
-		 * */
-		TokenizerEntry defaultTokenizer = new TokenizerEntry( tokenizerConfig, dic);
-		resourcePool.addTokenizerEntry("default", defaultTokenizer);
-		resourcePool.addTokenizerEntry("sensitive", defaultTokenizer);
+		 * create the JcsegTaskConfig instance according to the defination config
+		*/
+		if ( globalConfig.has("config") ) 
+		{
+			JSONObject configSetting = globalConfig.getJSONObject("config");
+			String[] configNames = JSONObject.getNames(configSetting);
+			for ( String name : configNames ) 
+			{
+				JSONObject configJson = configSetting.getJSONObject(name);
+				
+				//clone the globalJcsegTaskConfig
+				//and do the override working by local defination
+				JcsegTaskConfig config = globalJcsegTaskConfig.clone();
+				if ( configJson.length() > 0 ) {
+					resetJcsegTaskConfig(config, configJson);
+				}
+				
+				//register the global resource
+				resourcePool.addConfig(name, config);
+			}
+		}
+		
+		/*
+		 * create the tokenizer instance according the defination of tokenizer
+		*/
+		if ( globalConfig.has("tokenizer") )
+		{
+			JSONObject tokenizerSetting = globalConfig.getJSONObject("tokenizer");
+			String[] tokenizerNames = JSONObject.getNames(tokenizerSetting);
+			for ( String name : tokenizerNames )
+			{
+				JSONObject tokenizerJson = tokenizerSetting.getJSONObject(name);
+				
+				int algorithm = tokenizerJson.has("algorithm") 
+						? tokenizerJson.getInt("algorithm") : JcsegTaskConfig.COMPLEX_MODE;
+				
+				if ( ! tokenizerJson.has("dict") ) {
+					throw new JcsegException("Missing dict setting for tokenizer " + name);
+				}
+				if ( ! tokenizerJson.has("config") ) {
+					throw new JcsegException("Missing config setting for tokenizer " + name);
+				}
+				
+				 
+			}
+		}
 		
 		return this;
 	}
 	
+
+	/**
+	 * reset a JcsegTaskConfig from a JSONObject
+	 * 
+	 *  @param	config
+	 *  @param	json
+	*/
+	private void resetJcsegTaskConfig(JcsegTaskConfig config, JSONObject json)
+	{
+		if ( json.has("jcseg_maxlen") ) {
+			config.setMaxLength(json.getInt("jcseg_maxlen"));
+		}
+		if ( json.has("jcseg_icnname") ) {
+			config.setICnName(json.getBoolean("jcseg_icnname"));
+		}
+		if ( json.has("jcseg_mixcnlen") ) {
+			config.setMixCnLength(json.getInt("jcseg_mixcnlen"));
+		}
+		if ( json.has("jcseg_pptmaxlen") ) {
+			config.setPPT_MAX_LENGTH(json.getInt("jcseg_pptmaxlen"));
+		}
+		if ( json.has("jcseg_cnmaxlnadron") ) {
+			config.setMaxCnLnadron(json.getInt("jcseg_cnmaxlnadron"));
+		}
+		if ( json.has("jcseg_clearstopword") ) {
+			config.setClearStopwords(json.getBoolean("jcseg_clearstopword"));
+		}
+		if ( json.has("jcseg_cnnumtoarabic") ) {
+			config.setCnNumToArabic(json.getBoolean("jcseg_cnnumtoarabic"));
+		}
+		if ( json.has("jcseg_cnfratoarabic") ) {
+			config.setCnFactionToArabic(json.getBoolean("jcseg_cnfratoarabic"));
+		}
+		if ( json.has("jcseg_keepunregword") ) {
+			config.setKeepUnregWords(json.getBoolean("jcseg_keepunregword"));
+		}
+		if ( json.has("jcseg_ensencondseg") ) {
+			config.setEnSecondSeg(json.getBoolean("jcseg_ensencondseg"));
+		}
+		if ( json.has("jcseg_stokenminlen") ) {
+			config.setSTokenMinLen(json.getInt("jcseg_stokenminlen"));
+		}
+		if ( json.has("jcseg_nsthreshold") ) {
+			config.setNameSingleThreshold(json.getInt("jcseg_nsthreshold"));
+		}
+		if ( json.has("jcseg_keeppunctuations") ) {
+			config.setKeepPunctuations(json.getString("jcseg_keeppunctuations"));
+		}
+	}
 
 	/**
 	 * start the server 
@@ -194,15 +331,15 @@ public class JcsegServer
 
 	public static void main(String[] args) 
 	{
-		JcsegServerConfig config = new JcsegServerConfig();
-		JcsegServer server = new JcsegServer(config);
-		
 		try {
+			JcsegServerConfig config = new JcsegServerConfig();
+			config.resetFromFile("/java/JavaSE/jcseg/jcseg-server.properties");
+			JcsegServer server = new JcsegServer(config);
 			System.out.print("+--[Info]: Register handler ... ");
 			server.registerHandler();
 			System.out.println(" --[Ok]");
 			System.out.print("+--[Info]: Register global resource ... ");
-			server.registerGlobalResource();
+			server.registerGlobalResource(config.getGlobalConfig());
 			System.out.println(" --[Ok]");
 			server.start();
 		} catch (Exception e) {
