53a91b889744381d5da6973d37b389e822581ee8
chenxin <chenxin619315@gmail.com>
2015-12-04 15:39:52 +0800

    add serverConfig and globalResource base class

--- jcseg-server/src/main/java/org/lionsoul/jcseg/server/core/GlobalResource.java
+++ jcseg-server/src/main/java/org/lionsoul/jcseg/server/core/GlobalResource.java
@@ -1,142 +1,37 @@
-package org.lionsoul.jcseg.server;
+package org.lionsoul.jcseg.server.core;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.lionsoul.jcseg.tokenizer.core.ADictionary;
-import org.lionsoul.jcseg.tokenizer.core.JcsegTaskConfig;
-
 /**
- * global resource pool that manager the
- * dictionray, configuration, ISegment instance of jcseg.
+ * global resource class
  * 
  * @author chenxin<chenxin619315@gmail.com>
 */
-public class GlobalResourcePool 
+public abstract class GlobalResource 
 {
 	/**
-	 * dictionary pool 
+	 * global resource mapping  
 	*/
-	private Map<String, ADictionary> dicPool = null;
+	protected Map<Object, Object> resource = null;
 	
-	/**
-	 * JcsegTaskConfig pool 
-	*/
-	private Map<String, JcsegTaskConfig> configPool = null;
-	
-	/**
-	 *  TokenizerEntry pool
-	 * */
-	private Map<String, TokenizerEntry> tokenizerPool = null;
-	
-	
-	/**
-	 * construct method 
-	*/
-	public GlobalResourcePool()
+	public GlobalResource()
 	{
-		dicPool = new HashMap<String, ADictionary>();
-		configPool = new HashMap<String, JcsegTaskConfig>();
-		tokenizerPool = new HashMap<String, TokenizerEntry>();
-	}
-	
-	
-	/**
-	 * add a new tokenizer entry to entry pool
-	 * 
-	 * @param name
-	 * @param entry
-	 * */
-	public void addTokenizerEntry( String name, TokenizerEntry entry)
-	{
-	    tokenizerPool.put(name, entry);
-	}
-	
-	
-	/**
-	 *  remove a tokenizer entry from Tokenizer pool with specified name
-	 * 
-	 * @param name
-	 * */
-	public void removeTokonizerEntry(String name)
-	{
-	    tokenizerPool.remove( name );
+		resource = Collections.synchronizedMap(new HashMap<Object, Object>());
 	}
 
-	
-	/**
-	 * get tokenizer entry with specified name
-	 * 
-	 * @param  name
-	 * @return TokenizerEntry
-	 * */
-	public TokenizerEntry getTokenizerEntry( String name)
-	{
-	    return tokenizerPool.get(name);
+	public Object getResource(String name) {
+		return resource.get(name);
+	}
+
+	public void addResource(Object key, Object value) {
+		resource.put(key, value);
 	}
 	
-	
-	/**
-	 * add a new Dictionary instance mapping
-	 * 
-	 * @param	name
-	 * @param	dic
-	*/
-	public void addDict(String name, ADictionary dic)
+	public void removeResource(Object key)
 	{
-		dicPool.put(name, dic);
+		resource.remove(key);
 	}
 	
-	/**
-	 * remove specifield dictionary mapping
-	 * 
-	 * @param	name
-	*/
-	public void removeDict(String name)
-	{
-		dicPool.remove(name);
-	}
-	
-	/**
-	 * get the specifield dictionary
-	 * 
-	 * @param	name
-	 * @return	ADictionary
-	*/
-	public ADictionary getDic(String name)
-	{
-		return dicPool.get(name);
-	}
-	
-	/**
-	 * add a new JcsegTaskConfig mapping
-	 * 
-	 * @param	name
-	 * @param	config
-	*/
-	public void addConfig(String name, JcsegTaskConfig config)
-	{
-		configPool.put(name, config);
-	}
-	
-	/**
-	 * remove a specifield JcsegTaskConfig mapping
-	 * 
-	 * @param	name
-	*/
-	public void removeConfig(String name)
-	{
-		configPool.remove(name);
-	}
-	
-	/**
-	 * get the specifield config
-	 * 
-	 * @param	name
-	 * @return	JcsegTaskConfig
-	*/
-	public JcsegTaskConfig getConfig(String name)
-	{
-		return configPool.get(name);
-	}
 }
--- jcseg-server/src/main/java/org/lionsoul/jcseg/server/core/ServerConfig.java
+++ jcseg-server/src/main/java/org/lionsoul/jcseg/server/core/ServerConfig.java
@@ -1,100 +1,67 @@
-package org.lionsoul.jcseg.server;
-
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URL;
-
-import javax.imageio.ImageIO;
+package org.lionsoul.jcseg.server.core;
 
 /**
- * jcseg server configuration class
+ * server configuration class
  * 
  * @author chenxin<chenxin619315@gmail.com>
 */
-public class JcsegServerConfig 
+public class ServerConfig 
 {
+	public static final String JETTY_DEFAULT_CHARSET = "iso-8859-1";
+	
 	/**
 	 * access host 
 	*/
-	private String host = null;
+	protected String host = null;
 	
 	/**
 	 * server port
 	*/
-	private int port = 8080;
+	protected int port = 8080;
 	
 	/**
 	 * http idle timeout in millseconds
 	*/
-	private long httpIdleTimeout = 30000;
+	protected long httpIdleTimeout = 30000;
 	
 	/**
 	 * output buffer size 
 	*/
-	private int outputBufferSize = 32768;
+	protected int outputBufferSize = 32768;
 	
 	/**
 	 * request header size 
 	*/
-	private int requestHeaderSize = 8192;
+	protected int requestHeaderSize = 8192;
 	
 	/**
 	 * response header size 
 	*/
-	private int responseHeaderSize = 8192;
+	protected int responseHeaderSize = 8192;
 	
 	/**
 	 * max thread size in the thread pool 
 	*/
-	private int maxThreadPoolSize = 200;
+	protected int maxThreadPoolSize = 200;
 	
 	/**
 	 * max thread idle time in ms 
 	*/
-	private int threadIdleTimeout = 60000;
+	protected int threadIdleTimeout = 60000;
 	
 	/**
 	 * static resouce base path 
 	*/
-	private String appBasePath = null;
+	protected String appBasePath = null;
 	
 	/**
 	 * default charset 
 	*/
-	private String charset = null;
+	protected String charset = null;
 	
-	/**
-	 * favicon ico bytes 
-	*/
-	private byte[] favicon = null;
-	
-	/**
-	 * create and initialize the server config
-	 * 
-	 * @return	JcsegServerConfig
-	*/
-	public static JcsegServerConfig create()
+	public ServerConfig()
 	{
-		JcsegServerConfig self = new JcsegServerConfig();
-		try {
-			URL imageUrl = self.getClass().getResource("/res/logo.jpg");
-			if ( imageUrl != null ) {
-				BufferedImage image = ImageIO.read(imageUrl);
-				ByteArrayOutputStream bos = new ByteArrayOutputStream();
-				ImageIO.write(image, "jpg", bos);
-				self.setFavicon(bos.toByteArray());
-			}
-		} catch (IOException e) {
-			//e.printStackTrace();
-		}
-		
-		return self;
-	}
-	
-	private JcsegServerConfig()
-	{
-		this.charset = "utf-8";
+		charset = "utf-8";
 	}
 
 	public String getHost() {
@@ -176,13 +143,15 @@ public class JcsegServerConfig
 	public void setCharset(String charset) {
 		this.charset = charset;
 	}
-
-	public byte[] getFavicon() {
-		return favicon;
-	}
-
-	public void setFavicon(byte[] favicon) {
-		this.favicon = favicon;
-	}
 	
+	/**
+	 * check the server use the default iso-8859-1 chaset 
+	 * 
+	 * @return	boolean
+	*/
+	public boolean isDefaultCharset()
+	{
+		return (charset.length() == JETTY_DEFAULT_CHARSET.length()
+				&& charset.charAt(0) == JETTY_DEFAULT_CHARSET.charAt(0));
+	}
 }
