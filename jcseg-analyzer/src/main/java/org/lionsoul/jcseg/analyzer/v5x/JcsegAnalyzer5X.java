5861f65d7af2e405b6b9961c4e80603b910f7883
lionsoul <chenxin619315@gmail.com>
2016-05-21 21:12:31 +0800

    add Analyzer5x#(mode, JcsegTaskConfig) interface to make the analyzer more flexible

--- jcseg-analyzer/src/main/java/org/lionsoul/jcseg/analyzer/v5x/JcsegAnalyzer5X.java
+++ jcseg-analyzer/src/main/java/org/lionsoul/jcseg/analyzer/v5x/JcsegAnalyzer5X.java
@@ -21,21 +21,41 @@ public class JcsegAnalyzer5X extends Analyzer
     private JcsegTaskConfig config = null;
     private ADictionary dic = null;
     
+    /**
+     * initialize the analyzer with the specifield mode
+     * And jcseg will look for the default configuration file
+     * 
+     * @param   mode tokenizer mode
+     * @see     org.lionsoul.jcseg.core.JcsegTaskConfig
+    */
     public JcsegAnalyzer5X( int mode ) 
     {
-        this.mode = mode;
-        
-        //initialize the task config and the dictionary
-        config = new JcsegTaskConfig();
-        dic = DictionaryFactory.createSingletonDictionary(config);
+        this(mode, new JcsegTaskConfig());
     }
     
+    /**
+     * initialize the analyzer with the specifield mode
+     *  and the jcseg.properties file 
+     *  
+     * @param   mode tokenizer mode
+     * @param   proFile path of jcseg.properties file
+    */
     public JcsegAnalyzer5X( int mode, String proFile )
     {
-        this.mode = mode;
-        
-        config = new JcsegTaskConfig(proFile);
-        dic = DictionaryFactory.createSingletonDictionary(config);
+        this(mode, new JcsegTaskConfig(proFile));
+    }
+    
+    /**
+     * initialize the analyzer with the specifield mode and configuration
+     * 
+     * @param   mode tokenizer mode
+     * @param   config
+    */
+    public JcsegAnalyzer5X(int mode, JcsegTaskConfig config)
+    {
+        this.mode   = mode;
+        this.config = config;
+        this.dic    = DictionaryFactory.createSingletonDictionary(config);
     }
     
     public void setConfig( JcsegTaskConfig config ) 
