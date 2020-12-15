5a2b3071ebf2b604ff26ca1f592ac2e9b1fc75b7
Lion <lionsoul>
2020-12-15 22:24:56 +0800

    Get ready for the 2.6.3 alpha version

--- jcseg-core/src/main/java/org/lionsoul/jcseg/dic/ADictionary.java
+++ jcseg-core/src/main/java/org/lionsoul/jcseg/dic/ADictionary.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.net.URLDecoder;
 import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.Collections;
